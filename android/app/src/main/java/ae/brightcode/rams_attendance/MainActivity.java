a1f0890881e2f740b0b1cd00faf3d331c0c814d1
Muhannad Nasri <35005004+Muhannadnasri@users.noreply.github.com>
2021-11-15 11:00:32 +0400

    Upload

---
+++ android/app/src/main/java/ae/brightcode/rams_attendance/MainActivity.java
@@ -0,0 +1,6 @@
+package com.bajajuae.bajaj;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/kotlin/com/bajajuae/bajajuae_attendance/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bajajuae.bajajuae_attendance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
